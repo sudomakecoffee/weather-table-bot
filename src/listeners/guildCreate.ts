18338982417532687bb14b98da4b1d2b7c44627b
sudomakecoffee <brisk.home1381@sudomakecoffee.io>
2022-04-02 07:55:31 -0400

    Remove channel creation, add command creation

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -1,23 +1,16 @@
-/**
- * Fires whenever the bot joins a guild (server).
- */
 import { Client } from "discord.js";
 import { Commands } from "../commands";
 import BotConfig from "../botConfig";
 
 export default (client: Client): void => {
-  console.log("registering listener for guildCreate");
+  /**
+   * Fires whenever the bot joins a guild (server).
+   */
   client.on("guildCreate", async function (guild) {
     // when we join for the first time, create our commands
-    console.log(`Joined ${guild.name}`);
-
     const config = BotConfig.getInstance().config;
     if (!config.has(guild.id)) {
-      console.log(`registering ${Commands.length} commands in ${guild.name}:`);
-      Commands.forEach((command) => {
-        console.log(`\t${command.name}`);
-      });
-      await client.application?.commands.set(Commands, guild.id);
+      guild.commands.set(Commands);
 
       config.set(guild.id, {
         channelId: "",
@@ -25,24 +18,5 @@ export default (client: Client): void => {
       });
       BotConfig.getInstance().save(config);
     }
-
-    // if (guild.me?.permissions.has("MANAGE_CHANNELS")) {
-    //   const channelParent = guild.channels.cache.find(
-    //     (channel) =>
-    //       channel.name.toLowerCase().startsWith("stats") ||
-    //       channel.name.toLowerCase() === "voice"
-    //   );
-    //   const channel = await guild.channels.create("Weather: ", {
-    //     type: ChannelTypes.GUILD_VOICE,
-    //     parent: channelParent?.id,
-    //     permissionOverwrites: [
-    //       {
-    //         id: guild.roles.everyone,
-    //         allow: ["VIEW_CHANNEL"],
-    //         deny: ["SEND_MESSAGES", "READ_MESSAGE_HISTORY", "CONNECT"],
-    //       },
-    //     ],
-    //   });
-    // }
   });
 };
