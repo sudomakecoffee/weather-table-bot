05118b14725c1b3f5b1742e7656efbc59147eba0
sudomakecoffee <brisk.home1381@sudomakecoffee.io>
2022-04-03 08:52:07 -0400

    Add logging to file for posterity

--- src/logger.ts
+++ src/logger.ts
@@ -2,4 +2,8 @@ import { pino } from "pino";
 import dotenv from "dotenv";
 
 dotenv.config();
-export const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+const transport = pino.transport({
+  target: "pino/file",
+  options: { destination: "weatherbot.log" },
+});
+export const logger = pino({ level: process.env.LOG_LEVEL || "info" }, transport);
